8162272a1062627d2cbce1ee7e9a8a45036eedca
Ngo Hai <longphimon003@gmail.com>
2026-03-21 19:58:31 +0700

    first commit

---
+++ DoAnPhatTrienUngDungJ2EE/src/main/java/com/example/DoAnPhatTrienUngDungJ2EE/DoAnPhatTrienUngDungJ2EeApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoAnPhatTrienUngDungJ2EE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnPhatTrienUngDungJ2EeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoAnPhatTrienUngDungJ2EeApplication.class, args);
+	}
+
+}
---
+++ DoAnPhatTrienUngDungJ2EE/src/test/java/com/example/DoAnPhatTrienUngDungJ2EE/DoAnPhatTrienUngDungJ2EeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DoAnPhatTrienUngDungJ2EE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnPhatTrienUngDungJ2EeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
